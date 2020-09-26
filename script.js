d666f87c3156ad0a817da0ae0179292ff6a44a5d
Karencita09 <karencita0523@gmail.com>
2020-09-26 12:36:44 -0500

    Karen Vargas, Artista

---
+++ script.js
@@ -0,0 +1,29 @@
+function aventura() {
+        let name = prompt("¿Cual es el nombre  de la banda?");
+        let pais = prompt("¿ De que País son ?");
+        let genero = prompt("¿A que género de musica pertenece la banda?");
+        let cancion = prompt("Canción favorita de la banda");
+        let nombre = document.getElementById("nombre")
+        nombre.innerHTML = "Su nombre es: " + name  + "<br>" + "Su país es: " + pais + "<br>"  + "Su genero es: " + genero + "<br>"  + "Una canción es: " + cancion
+}
+function yiyo() {
+    let name = prompt("¿Cual es el nombre del Artista'");
+        let pais = prompt("¿De que País es?");
+        let genero = prompt("¿A que género de musica pertenecen?");
+        let cancion = prompt("Canción favorita del artista");
+        let nombre = document.getElementById("nombre")
+        nombre1.innerHTML = "Su nombre es: " + name  + "<br>" + "Su país es: " + pais + "<br>"  + "Su genero es: " + genero + "<br>"  + "Una canción es: " + cancion
+    
+}
+
+function adolecentes() {
+         
+    let name = prompt("¿Cual es el nombre  de la banda?");
+    let pais = prompt("¿ De que País son ?");
+    let genero = prompt("¿A que género de musica pertenece la banda?");
+    let cancion = prompt("Canción favorita de la banda");
+    let nombre = document.getElementById("nombre")
+        nombre2.innerHTML = "Su nombre es: " + name  + "<br>" + "Su país es: " + pais + "<br>"  + "Su genero es: " + genero + "<br>"  + "Una canción es: " + cancion
+
+    
+}
